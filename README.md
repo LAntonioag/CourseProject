9ed26d5f3c920836b28c452b3f9274dd516393d7
Luis Antonio <wayrapaq@gmail.com>
2014-06-22 16:45:10 -0500

    4th commit

--- README.md
+++ README.md
@@ -1,3 +1,64 @@
-CourseProject
+Introduction
 =============
-The script perform a merge over a two datasets [Test and Train datasets],  after that 
+The script perform a merge over a two datasets [Test and Train datasets],  after that create a n
+
+Enviroment Information
+======================
+This script was created in Windows 8, and was tested in the same environment; for other operative systems the use of "/" character for move across directories maybe not work.
+
+Script Description - run_analysis.R
+===================================
+The scripts take 8 steps in order to obtain the tidy dataset, this steps are:
+
+#### 	1. 	Download the Datasets:
+Using the download.file function the script download the database from this url: <https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip>; and unzip it in the current working directory.
+
+####	2.	Set the work directory:
+Here the we save the current working directory in a variable called "dir", because at the end we will be write the final output there, and change to the unzip directory where the files are (in the "UCI HAR Dataset" directory).
+
+####	3.	Reading the files:
+In this step the require files are read and load for the processing, and are stored into local varibles with the same names; this files are:
+- Test files:
+	- X_test.txt
+	- y_test.txt
+	- subject_test.txt
+
+- Train files:
+	- X_train.txt
+	- y_train.txt
+	- subject_train.txt
+
+- Features file
+	- "features.txt
+
+- Activity label file:
+	- activity_labels.txt
+
+####	4.	Combine and merge [train and test] the datasets:
+First we merge using the rbind function the datasets in the follwing form:
+- x_data dataset:
+	- Obtaing from merging the train_x and test_x datasets.
+
+- Subject dataset:
+	- Obtaing from merging the subject_train and subject_test datasets.
+
+- Activity dataset:
+	- Obtaing from merging the train_y and test_y datasets.
+
+After combine the 3 datasets, we create a new dataframe called raw_data; that is compouse from:
+- raw_data dataset:
+	- Obtaing from merging the x_data, subject and activity datasets.
+
+And using the features dataset we change the name of the variables in the raw_data set in order to process it later.
+
+####	5. Extracts the measurements on the mean and standard deviation
+In this step we use the grep function to extract only the mean() and std() variables from the raw_data, and the result is storaged in a new data frame called tidy_dataset.
+
+####	6. Apply descriptive activity names to name the activities
+Using the labels from the activity_labels we change the representation of the activity from numeric to character; and apply to all the observations in the tidy_dataset.
+
+#### 	7.	Appropriately labels the data set with descriptive variable names:
+In this step we realize some clean in the variable names; so we remove some "noise" characters like "-" and "()"; for do that we use gsub function for remove that characters from the tidy_dataset data frame.
+		
+####	8.	Creating a second, independent tidy data set with the average of each variable for each activity and each subject:
+We use the reshape2 library to create a second tidy data set; for do that we firts use the melt function over the tidy_dataset and create a new data using Subject and Activity variables as ID; the result data frame is called data_melt; and using the dcast function we create a new data set from the data_melt that store the mean of each variable according the Subject and Activity variables; this second result is called tidyData; and finally we write the result in a new txt file called "tidyDataSet.txt".
--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,24 @@
-# Donwload the Dataset ----------------------------------------------------
+##Downloading the Dataset -------------------------------------------------
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
               paste(getwd(),"/dataset.zip",sep=""))
-unzip("dataset.zip")
+unzip("dataset.zip") # Unzip the dataset in the current directory
 
-# Set the work directory --------------------------------------------------
-root_dir <-getwd()
+##Set the work directory --------------------------------------------------
+root_dir <-getwd() # store the root dir where the final result will be saved
 work_dir <-paste(getwd(),"/UCI HAR Dataset",sep="")
-setwd(work_dir)
+setwd(work_dir) # change to the UCI HAR Dataset folder
 
-
-# Read the files
-test_dir <- paste(work_dir,"/test",sep="")
-setwd(test_dir)
-# Read the Test files
+##Reading the files -------------------------------------------------------
+# Read the Test files [x_test, y_test and subject_test]
+test_dir <- paste(work_dir,"/test",sep="") 
+setwd(test_dir) # change to the test directory
 test_x <- read.table("X_test.txt")
 test_y <- read.table("y_test.txt")
 subject_test <- read.table("subject_test.txt")
 
-# Read the Train files
+# Read the Train files [x_train, y_train and subject_train]
 train_dir <- paste(work_dir,"/train",sep="")
-setwd(train_dir)
+setwd(train_dir) # change to the train directory
 train_x <- read.table("X_train.txt")
 train_y <- read.table("y_train.txt")
 subject_train <- read.table("subject_train.txt")
@@ -31,34 +30,31 @@ features <- read.table("features.txt")
 # Read the activity_labels file
 setwd(work_dir)
 activity_labels <- read.table("activity_labels.txt")
-activity_labels[,2] <- as.character(activity_labels[,2])
-# Combine the two DataSets
-#x_data <- merge(train_x,test_x,by.x=names(train_x),by.y=names(test_x),all=TRUE)
-#subject <-  merge(subject_train,subject_test,by.x=names(subject_train),by.y=names(subject_test),all=TRUE)
+activity_labels[,2] <- as.character(activity_labels[,2]) # cast as character
 
+
+##Combine and merge [train and test] the datasets -------------------------
+
+
+##Combine and merge the datasets ------------------------------------------
+# Combine each dataset with his respectivly pair
 x_data <- rbind(train_x,test_x)
 subject <-  rbind(subject_train,subject_test)
 activity <- rbind(train_y, test_y)
 
-
-## Merging the Test and Train DataSets
+#Merging the Test and Train DataSets
 raw_data <- data.frame(x_data, subject, activity)
 
-# Label the Data Sets
-
+#Label the Data Sets
 names_data <- c(as.character(features[,2]), "Subject","Activity")
 names(raw_data) = names_data
 
 # 2. Extracts the measurements on the mean and standard deviation  --------
-# Get the specific columns with the grep command 81
-# index_list <- grep("(.*)(mean(.*)|std(.*)|Subject|Activity)(.*)", names(raw_data))
-# Get 68 Variables
 index_list <- grep("(.*)(-mean[()]|-std[()])|(Subject|Activity)", names(raw_data))
 
 # Then subset the dataset
 tidy_dataset <- raw_data[,c(index_list)]
 
-# For 68 Variables
 # 3. Uses descriptive activity names to name the activities  --------------
 tidy_dataset[,68] <- as.character(tidy_dataset[,68])
 
@@ -73,15 +69,12 @@ for (k in c(1:dim(tidy_dataset)[1])) {
     )
 }
 
-# Remove Noise characters "() and -"
-# gsub("(-|[()])", "", "fBodyGyro-std()-X")
-
-
 # 4. Appropriately labels the data set with descriptive variable n --------
-
+# Remove Noise characters "() and -"
 names_list <- names(tidy_dataset)
 # gsub("(-|[()])", "", names_list)
 names(tidy_dataset) <- gsub("(-|[()])", "", names_list)
+
 
 # First apply the melt function [for 68 variables]
 library(reshape2) # Load the reshape2 library
